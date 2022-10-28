9bbfed4365decee6f9f290dd0bdcef03b7e44b0f
sudiptdh <sudipta.a.dhar@capgemini.com>
2022-10-28 22:56:05 +0530

    new commit

--- src/main/java/com/sudipta/practice/git_practice/App.java
+++ src/main/java/com/sudipta/practice/git_practice/App.java
@@ -7,10 +7,13 @@ package com.sudipta.practice.git_practice;
 public class App 
 {
 	public void addData(String data) {
+		
 		System.out.println("data = "+ data);
 	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        App app= new App();
+        app.addData("some-data");
     }
 }
